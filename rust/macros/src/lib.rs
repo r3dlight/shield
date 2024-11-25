6b387db6a023a2d53117ca098c685499e2c61ca0
Florent Valette <florent.valette@ledger.fr>
2024-11-25 15:20:39 +0100

    rust shield macro: add shield-startup-macros crate
    Add proc macro crate with helper macro for entrypoint  generation.
    
    Ideally, we shall use an attribute macro instead of function like macro
    and use it the same way rustc w/ std is doing.
    I.e. an inner attribute which generates entrypoint and call the crate
    main (i.e. crate::main w/ name mangling) function. *BUT* inner attribute
    macro are not stable yet ¯\_(ツ)_/¯
    
    To generate startup code in order to call mangled main from _start use
    the following:
    
     ```
     #![no_std]
     #![no_main]
     extern crate shield;
     shield_main!():
     fn main() {
         [...]
     }
     ```
    
    Once stable, it shall be like this :
     ```
     #![cfg_attr(cfg(sentry_backend), no_std]
     #![cfg_attr(cfg(sentry_backend), no_main]
     #![cfg_attr(cfg(sentry_backend), shield_main]
     ```

---
+++ rust/macros/src/lib.rs
@@ -0,0 +1,58 @@
+// SPDX-FileCopyrightText: 2024 Ledger SAS
+//
+// SPDX-License-Identifier: Apache-2.0
+
+//! # shield-macros
+//!
+//! Procedural macros collection for generating startup glue between task entrypoint and
+//! user defined main.
+//! For Rust/Cargo based tasks, boot flow is the following:
+//! `_start` --> `main` --> `crate::main`
+//!  - `_start` is the task entrypoint, symbol name is mandatory and resolve at buildtime
+//!    in by barbican after tasks relocation.
+//!  - `main` (w/o mangling) is called by `_start` after shield initialization. This
+//!    symbol must be able to resolve crate::main, i.e. main that is defined in task
+//!    `main.rs` file.
+
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse, parse_macro_input};
+
+/// Generate shield entrypoint function
+///
+/// # Usage
+///
+/// Procedural macro that generates a not mangled `main` function that call user
+/// defined main. This macro takes no arguments and must be call from `main.rs` file.
+///
+/// > **NOTE**: The function is generated in an inner module named `__shield_startup`
+/// > **TODO**: use an inner attribute macro once stable (this is still a nightly feature)
+///
+/// # Example
+//
+/// ```rust
+/// #![no_std]
+/// #![no_main]
+/// extern crate shield;
+///
+/// shield_main!():
+///
+/// fn main() {
+///     [...]
+/// }
+/// ```
+#[proc_macro]
+pub fn shield_main(tokens: TokenStream) -> TokenStream {
+    let _ = parse_macro_input!(tokens as parse::Nothing);
+    quote! {
+        #[doc(hidden)]
+        mod __shield_startup {
+            #[doc(hidden)]
+            #[no_mangle]
+            pub fn main() { crate::main() }
+        }
+    }
+    .into()
+}
