5adceb040127e390df12bb81873db8c393c63590
Florent Valette <florent.valette@ledger.fr>
2025-01-03 11:48:25 +0100

    rust: add dual licensing to rust sources

--- rust/examples/simple_main.rs
+++ rust/examples/simple_main.rs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2024 Ledger SAS
 //
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
 #![cfg_attr(target_os = "none", no_std)]
 #![cfg_attr(target_os = "none", no_main)]
